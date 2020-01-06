eb76822e84ad1713a09bc8e22700e276cda704db
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2020-01-06 20:13:59 -0300

    agregamos cantidad de shifts como entrada al shifter

--- src/signed_shifter.vhdl
+++ src/signed_shifter.vhdl
@@ -5,11 +5,13 @@ use ieee.numeric_std.all;
 entity signed_shifter is
 
 	generic (
-		W: integer := 10
+		W					:	integer := 10;
+		POSITIONS_W			:	integer := 4
 	);
 	port (
-		input_vector:		in signed( W - 1 downto 0);
-		shifted_vector:		out signed( W - 1 downto 0)
+		input_vector		:	in signed( W - 1 downto 0 );
+		shift_positions		:	in unsigned( POSITIONS_W - 1 downto 0 );
+		shifted_vector		:	out signed( W - 1 downto 0 )
 	);
 
 end entity signed_shifter;
@@ -17,6 +19,6 @@ end entity signed_shifter;
 architecture behavioral of signed_shifter is
 begin
 
-	shifted_vector	<=	shift_right(input_vector, 1);
+	shifted_vector	<=	shift_right(input_vector, to_integer(shift_positions));
 
 end architecture behavioral;
