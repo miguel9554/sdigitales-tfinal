7f537e6da9182287ca9555d30a491c7e133a8c40
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2020-01-13 21:59:15 -0300

    fix names

--- utils/stimulus_generation/addsub.py
+++ utils/stimulus_generation/addsub.py
@@ -6,7 +6,14 @@ def round(number):
     return int(math.floor(number))
 
 
-filepath = '../../verification/tb_signed_shifter/stimulus.dat'
+def overflow(number, width):
+    if -2**(width-1) <= number <= 2**(width-1)-1:
+        return False
+    else:
+        return True
+
+
+filepath = '../../verification/tb_addsub/stimulus.dat'
 # Number of test cases to generate
 NUMBER_OF_TESTS = 10000
 # Deterministic seed for reproducibility
@@ -14,20 +21,22 @@ random.seed(54)
 
 # Width, in bits, of general values
 WIDTH = 10
-# Width, in bits, of number of shifts
-SHIFT_WIDTH = 4
 
 with open(filepath, 'w') as fp:
-    fp.write(f"# input_vector shift_positions expected_result\n")
+    fp.write(f"# operation first_input second_input expected_result\n")
     for _ in range(NUMBER_OF_TESTS):
 
-        input_value = random.randint(-2**(WIDTH-1), 2**(WIDTH-1)-1)
-        number_of_shifts = random.randint(0, 2**(SHIFT_WIDTH)-1)
+        first_input = random.randint(-2**(WIDTH-1), 2**(WIDTH-1)-1)
+        second_input = random.randint(-2**(WIDTH-1), 2**(WIDTH-1)-1)
+        
+        if random.randint(0, 1) == 1:
+            operation = '+'
+            expected_result = first_input + second_input
+        else:
+            operation = '-'
+            expected_result = first_input - second_input
 
-        expected_result = round(input_value/(2**number_of_shifts))
+        if overflow(expected_result, WIDTH):
+            continue
 
-        # The smallest number we can get right shifting a negative number is -1
-        if (input_value < 0) and (expected_result == 0):
-            expected_result = -1
-
-        fp.write(f"{input_value} {number_of_shifts} {expected_result}\n")
+        fp.write(f"{operation} {first_input} {second_input} {expected_result}\n")
--- utils/stimulus_generation/signed_shifter.py
+++ utils/stimulus_generation/signed_shifter.py
@@ -6,14 +6,7 @@ def round(number):
     return int(math.floor(number))
 
 
-def overflow(number, width):
-    if -2**(width-1) <= number <= 2**(width-1)-1:
-        return False
-    else:
-        return True
-
-
-filepath = '../../verification/tb_addsub/stimulus.dat'
+filepath = '../../verification/tb_signed_shifter/stimulus.dat'
 # Number of test cases to generate
 NUMBER_OF_TESTS = 10000
 # Deterministic seed for reproducibility
@@ -21,22 +14,20 @@ random.seed(54)
 
 # Width, in bits, of general values
 WIDTH = 10
+# Width, in bits, of number of shifts
+SHIFT_WIDTH = 4
 
 with open(filepath, 'w') as fp:
-    fp.write(f"# operation first_input second_input expected_result\n")
+    fp.write(f"# input_vector shift_positions expected_result\n")
     for _ in range(NUMBER_OF_TESTS):
 
-        first_input = random.randint(-2**(WIDTH-1), 2**(WIDTH-1)-1)
-        second_input = random.randint(-2**(WIDTH-1), 2**(WIDTH-1)-1)
-        
-        if random.randint(0, 1) == 1:
-            operation = '+'
-            expected_result = first_input + second_input
-        else:
-            operation = '-'
-            expected_result = first_input - second_input
+        input_value = random.randint(-2**(WIDTH-1), 2**(WIDTH-1)-1)
+        number_of_shifts = random.randint(0, 2**(SHIFT_WIDTH)-1)
 
-        if overflow(expected_result, WIDTH):
-            continue
+        expected_result = round(input_value/(2**number_of_shifts))
 
-        fp.write(f"{operation} {first_input} {second_input} {expected_result}\n")
+        # The smallest number we can get right shifting a negative number is -1
+        if (input_value < 0) and (expected_result == 0):
+            expected_result = -1
+
+        fp.write(f"{input_value} {number_of_shifts} {expected_result}\n")
