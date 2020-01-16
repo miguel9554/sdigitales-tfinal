b7b06b5b680b76a4b1a9cca80df6d7da30357a81
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2020-01-16 00:27:23 -0300

    corregidos los ángulos de entrada, no se estaba trabajando con el rango completo

--- utils/stimulus_generation/cordic.py
+++ utils/stimulus_generation/cordic.py
@@ -33,7 +33,7 @@ ANGLE_WIDTH = ANGLE_INTEGER_WIDTH + ANGLE_FRACTIONAL_WIDTH + 1
 PURE_CORDIC_SCALE_FACTOR = 0.607252935
 ROUNDED_CORDIC_SCALE_FACTOR = int(round(PURE_CORDIC_SCALE_FACTOR*2**(COORDINATES_WIDTH+OFFSET_VHDL_COORDS_WIDTH-1)))*2**-(COORDINATES_WIDTH+OFFSET_VHDL_COORDS_WIDTH-1)
 
-atan = [45,
+atan_degrees = [45,
 26.565051177078,
 14.0362434679265,
 7.1250163489018,
@@ -50,6 +50,8 @@ atan = [45,
 0.003497056850704,
 0.00174852842698]
 
+atan_fixed_point = [int(round(angle_degrees*2**ANGLE_FRACTIONAL_WIDTH)) for angle_degrees in atan_degrees]
+
 with open(filepath, 'w') as fp:
 
     fp.write("# X0 Y0 angle X Y\n")
@@ -58,7 +60,7 @@ with open(filepath, 'w') as fp:
 
         X0 = random.randint(-2**(COORDINATES_WIDTH-1), 2**(COORDINATES_WIDTH-1)-1)
         Y0 = random.randint(-2**(COORDINATES_WIDTH-1), 2**(COORDINATES_WIDTH-1)-1)
-        angle = random.randint(-2**(ANGLE_INTEGER_WIDTH-1), 2**(ANGLE_INTEGER_WIDTH-1)-1)
+        angle = random.randint(-2**(ANGLE_WIDTH-1), 2**(ANGLE_WIDTH-1)-1)
 
         X_old = X0
         Y_old = Y0
@@ -70,7 +72,7 @@ with open(filepath, 'w') as fp:
 
             X = X_old - floor_round(Y_old/(2**step)) if not sigma_old else X_old + floor_round(Y_old/(2**step))
             Y = Y_old + floor_round(X_old/(2**step)) if not sigma_old else Y_old - floor_round(X_old/(2**step))
-            Z = Z_old - atan[step] if not sigma_old else Z_old + atan[step]
+            Z = Z_old - atan_fixed_point[step] if not sigma_old else Z_old + atan_fixed_point[step]
             sigma = 0 if Z >= 0 else 1
 
             X_old = X
@@ -81,4 +83,4 @@ with open(filepath, 'w') as fp:
         X = floor_round(X*ROUNDED_CORDIC_SCALE_FACTOR)
         Y = floor_round(Y*ROUNDED_CORDIC_SCALE_FACTOR)
 
-        fp.write(f"{X0} {Y0} {angle*2**ANGLE_FRACTIONAL_WIDTH} {X} {Y}\n")
+        fp.write(f"{X0} {Y0} {angle} {X} {Y}\n")
