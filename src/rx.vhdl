8a8515f0d9e1a8a8d2a669ae846c50793a1dbda4
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2020-01-20 02:53:54 -0300

    rotador eje x

---
+++ src/rx.vhdl
@@ -0,0 +1,59 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Rx is
+
+    generic (
+        COORDS_WIDTH            : integer := 10;
+        ANGLES_INTEGER_WIDTH    : integer := 6;
+        ANGLES_FRACTIONAL_WIDTH : integer := 16;
+        STAGES                  : integer := 16
+    );
+    port (
+        X0, Y0, Z0      :   in signed(COORDS_WIDTH-1 downto 0);
+        angle           :   in signed(ANGLES_FRACTIONAL_WIDTH+ANGLES_INTEGER_WIDTH downto 0);
+        X, Y, Z         :   out signed(COORDS_WIDTH-1 downto 0)
+    );
+
+end entity Rx;
+
+architecture behavioral of Rx is
+
+    -- Cordic declaration
+    component cordic is
+        generic (
+            COORDS_WIDTH            : integer;
+            ANGLES_INTEGER_WIDTH    : integer;
+            ANGLES_FRACTIONAL_WIDTH : integer;
+            STAGES                  : integer
+        );
+        port (
+            X0, Y0          :   in signed(COORDS_WIDTH-1 downto 0);
+            angle           :   in signed(ANGLES_FRACTIONAL_WIDTH+ANGLES_INTEGER_WIDTH downto 0);
+            X, Y            :   out signed(COORDS_WIDTH-1 downto 0)
+        );
+    end component cordic;
+
+begin
+    
+    -- Cordic rotation
+    cordic_rotator: cordic
+        generic map (
+            COORDS_WIDTH            =>  COORDS_WIDTH,
+            ANGLES_INTEGER_WIDTH    =>  ANGLES_INTEGER_WIDTH,
+            ANGLES_FRACTIONAL_WIDTH =>  ANGLES_FRACTIONAL_WIDTH,
+            STAGES                  =>  STAGES
+        )
+        port map (
+            X0          =>  Y0,
+            Y0          =>  Z0,
+            angle       =>  angle,
+            X           =>  Y,
+            Y           =>  Z
+        );
+
+    -- X component stays the same
+    X   <=  X0;
+
+end architecture behavioral;
---
+++ utils/stimulus_generation/rx.py
@@ -0,0 +1,49 @@
+import random
+import math
+import pathlib
+import cordic
+
+
+def main():
+
+    filepath = pathlib.Path(__file__).parent.parent.parent.absolute() / 'verification' / 'tb_rx' / 'stimulus.dat'
+
+    # Deterministic seed for reproducibility
+    random.seed(54)
+
+    # Number of test cases to generate
+    NUMBER_OF_TESTS = 1000
+
+    # Width, in bits, of coordinates
+    COORDINATES_WIDTH = 30
+    # VHDL_COORDINATES_WIDTH - PYTHON_COORDINATES_WIDTH (how many more bits are used in the vhdl testbench)
+    OFFSET_VHDL_COORDS_WIDTH = 2
+
+    # Width, in bits, of the integer part of angles
+    ANGLE_INTEGER_WIDTH = 6
+    # Width, in bits, of the fractional part of angles
+    ANGLE_FRACTIONAL_WIDTH = 16
+    # Total width, in bits, of angles
+    ANGLE_WIDTH = ANGLE_INTEGER_WIDTH + ANGLE_FRACTIONAL_WIDTH + 1
+
+    cordic_instace = cordic.cordic(coords_width=COORDINATES_WIDTH, offset_coords_width=OFFSET_VHDL_COORDS_WIDTH, 
+        angle_integer_width=ANGLE_INTEGER_WIDTH, angle_fractional_width=ANGLE_FRACTIONAL_WIDTH)
+
+    with open(filepath, 'w') as fp:
+
+        fp.write("# X0 Y0 Z0 angle X Y Z\n")
+
+        for _ in range(NUMBER_OF_TESTS):
+
+            X0 = random.randint(-2**(COORDINATES_WIDTH-1), 2**(COORDINATES_WIDTH-1)-1)
+            Y0 = random.randint(-2**(COORDINATES_WIDTH-1), 2**(COORDINATES_WIDTH-1)-1)
+            Z0 = random.randint(-2**(COORDINATES_WIDTH-1), 2**(COORDINATES_WIDTH-1)-1)
+            angle = random.randint(-2**(ANGLE_WIDTH-1), 2**(ANGLE_WIDTH-1)-1)
+
+            Y, Z = cordic_instace.rotate(Y0, Z0, angle)
+            X = X0
+
+            fp.write(f"{X0} {Y0} {Z0} {angle} {X} {Y} {Z}\n")
+
+if __name__ == "__main__":
+    main()
