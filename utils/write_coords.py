03537a9a3dde4d51374d518d537973b0945f1ece
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2020-03-31 12:16:29 -0300

    correct values, rm old

--- utils/write_coords.py
+++ utils/write_coords.py
@@ -2,7 +2,7 @@ import serial
 import pathlib
 
 
-COORDS_WIDTH = 31
+COORDS_WIDTH = 32
 LINES_TO_SEND = 11946
 
 filepath = pathlib.Path(__file__).absolute().parent / 'coordenadas.txt' 
@@ -13,11 +13,11 @@ with open(filepath) as fp, serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as s
         
         if line_number < LINES_TO_SEND:
 
-            x = int(float(line.split('\t')[0])*2**COORDS_WIDTH)
+            x = int(float(line.split('\t')[0])*2**(COORDS_WIDTH-1))
             x_signed = True if x < 0 else False
-            y = int(float(line.split('\t')[1])*2**COORDS_WIDTH)
+            y = int(float(line.split('\t')[1])*2**(COORDS_WIDTH-1))
             y_signed = True if y < 0 else False
-            z = -1*int(float(line.split('\t')[2])*2**COORDS_WIDTH)
+            z = -1*int(float(line.split('\t')[2])*2**(COORDS_WIDTH-1))
             z_signed = True if z < 0 else False
 
             ser.write((x).to_bytes(4, 'big', signed=x_signed))
